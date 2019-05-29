2e8f29cb450312b1497b856386be982eb15eea10
XATTpy <zoomerrr@mail.ru>
2019-05-29 18:46:10 +0300

    variable names fixed

--- bars.py
+++ bars.py
@@ -8,42 +8,42 @@ def load_data(filepath):
 
 def get_biggest_bar(data):
     seats_count = []
-    for i in range(len(data["features"])):
-        seats_count.append(data["features"][i]["properties"]['Attributes']["SeatsCount"])
+    for bar_number in range(len(data["features"])):
+        seats_count.append(data["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
 
     biggest = seats_count.index(max(seats_count))
-    name = data["features"][biggest]["properties"]['Attributes']['Name']
-    print("Самый большой бар - %s." % name)
+    bar_name = data["features"][biggest]["properties"]['Attributes']['Name']
+    print("Самый большой бар - %s." % bar_name)
 
 
 def get_smallest_bar(data):
     seats_count = []
-    for i in range(len(data["features"])):
-        seats_count.append(data["features"][i]["properties"]['Attributes']["SeatsCount"])
+    for bar_number in range(len(data["features"])):
+        seats_count.append(data["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
 
     smallest = seats_count.index(min(seats_count))
-    name = data["features"][smallest]["properties"]['Attributes']['Name']
-    print("Самый маленький бар - %s." % name)
+    bar_name = data["features"][smallest]["properties"]['Attributes']['Name']
+    print("Самый маленький бар - %s." % bar_name)
 
 
 def get_closest_bar(data, longitude, latitude):
     x, y = longitude, latitude
     geo = []
-    for i in range(len(data["features"])):
-        place = data["features"][i]["geometry"]["coordinates"]
+    for bar_number in range(len(data["features"])):
+        place = data["features"][bar_number]["geometry"]["coordinates"]
         distance = abs(x-place[0]) + abs(y-place[1])
         geo.append(distance)
 
     closest = geo.index(min(geo))
-    name = data["features"][closest]["properties"]['Attributes']['Name']
-    print("Самый близкий бар - %s" % name)
+    bar_name = data["features"][closest]["properties"]['Attributes']['Name']
+    print("Самый близкий бар - %s" % bar_name)
 
 
 if __name__ == '__main__':
-    data = load_data('1796.json')
+    bars = load_data('1796.json')
     longitude = float(input("Введите вашу геопозицию(долготу): "))
     latitude = float(input("Введите вашу геопозицию(широту): "))
 
-    get_biggest_bar(data)
-    get_smallest_bar(data)
-    get_closest_bar(data, longitude, latitude)
+    get_biggest_bar(bars)
+    get_smallest_bar(bars)
+    get_closest_bar(bars, longitude, latitude)
