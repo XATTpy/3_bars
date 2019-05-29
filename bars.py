e2f7c0e5c6b54ad52383366296f1504d9c6fe29c
XATTpy <zoomerrr@mail.ru>
2019-05-29 18:50:59 +0300

    variable names fixed

--- bars.py
+++ bars.py
@@ -6,36 +6,36 @@ def load_data(filepath):
         return json.load(f)
 
 
-def get_biggest_bar(data):
+def get_biggest_bar(bars):
     seats_count = []
-    for bar_number in range(len(data["features"])):
-        seats_count.append(data["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
+    for bar_number in range(len(bars["features"])):
+        seats_count.append(bars["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
 
     biggest = seats_count.index(max(seats_count))
-    bar_name = data["features"][biggest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][biggest]["properties"]['Attributes']['Name']
     print("Самый большой бар - %s." % bar_name)
 
 
-def get_smallest_bar(data):
+def get_smallest_bar(bars):
     seats_count = []
-    for bar_number in range(len(data["features"])):
-        seats_count.append(data["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
+    for bar_number in range(len(bars["features"])):
+        seats_count.append(bars["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
 
     smallest = seats_count.index(min(seats_count))
-    bar_name = data["features"][smallest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][smallest]["properties"]['Attributes']['Name']
     print("Самый маленький бар - %s." % bar_name)
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     x, y = longitude, latitude
     geo = []
-    for bar_number in range(len(data["features"])):
-        place = data["features"][bar_number]["geometry"]["coordinates"]
+    for bar_number in range(len(bars["features"])):
+        place = bars["features"][bar_number]["geometry"]["coordinates"]
         distance = abs(x-place[0]) + abs(y-place[1])
         geo.append(distance)
 
     closest = geo.index(min(geo))
-    bar_name = data["features"][closest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][closest]["properties"]['Attributes']['Name']
     print("Самый близкий бар - %s" % bar_name)
 
 
