51b486f0be64d15d6999673ae912dd57e9d2bc7b
XATTpy <zoomerrr@mail.ru>
2019-05-29 18:20:26 +0300

    added functions and JSON file

--- bars.py
+++ bars.py
@@ -2,20 +2,48 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r") as f:
+        return json.load(f)
 
 
 def get_biggest_bar(data):
-    pass
+    seats_count = []
+    for i in range(len(data["features"])):
+        seats_count.append(data["features"][i]["properties"]['Attributes']["SeatsCount"])
+
+    biggest = seats_count.index(max(seats_count))
+    name = data["features"][biggest]["properties"]['Attributes']['Name']
+    print("Самый большой бар - %s." % name)
 
 
 def get_smallest_bar(data):
-    pass
+    seats_count = []
+    for i in range(len(data["features"])):
+        seats_count.append(data["features"][i]["properties"]['Attributes']["SeatsCount"])
+
+    smallest = seats_count.index(min(seats_count))
+    name = data["features"][smallest]["properties"]['Attributes']['Name']
+    print("Самый маленький бар - %s." % name)
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    x, y = longitude, latitude
+    geo = []
+    for i in range(len(data["features"])):
+        place = data["features"][i]["geometry"]["coordinates"]
+        distance = abs(x-place[0]) + abs(y-place[1])
+        geo.append(distance)
+
+    closest = geo.index(min(geo))
+    name = data["features"][closest]["properties"]['Attributes']['Name']
+    print("Самый близкий бар - %s" % name)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('1796.json')
+    longitude = float(input("Введите вашу геопозицию(долготу): "))
+    latitude = float(input("Введите вашу геопозицию(широту): "))
+
+    get_biggest_bar(data)
+    get_smallest_bar(data)
+    get_closest_bar(data, longitude, latitude)
