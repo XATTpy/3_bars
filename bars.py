d7ce98d13340f2f8880a26e77930f4575c3c0e61
XATTpy <zoomerrr@mail.ru>
2019-05-30 14:01:01 +0300

    fix quotes

--- bars.py
+++ bars.py
@@ -9,20 +9,20 @@ def load_data(filepath):
 def get_biggest_bar(bars):
     seats_count = []
     for bar_number in range(len(bars["features"])):
-        seats_count.append(bars["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
+        seats_count.append(bars["features"][bar_number]["properties"]["Attributes"]["SeatsCount"])
 
     biggest = seats_count.index(max(seats_count))
-    bar_name = bars["features"][biggest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][biggest]["properties"]["Attributes"]["Name"]
     print("Самый большой бар - %s." % bar_name)
 
 
 def get_smallest_bar(bars):
     seats_count = []
     for bar_number in range(len(bars["features"])):
-        seats_count.append(bars["features"][bar_number]["properties"]['Attributes']["SeatsCount"])
+        seats_count.append(bars["features"][bar_number]["properties"]["Attributes"]["SeatsCount"])
 
     smallest = seats_count.index(min(seats_count))
-    bar_name = bars["features"][smallest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][smallest]["properties"]["Attributes"]["Name"]
     print("Самый маленький бар - %s." % bar_name)
 
 
@@ -35,12 +35,12 @@ def get_closest_bar(bars, longitude, latitude):
         geo.append(distance)
 
     closest = geo.index(min(geo))
-    bar_name = bars["features"][closest]["properties"]['Attributes']['Name']
+    bar_name = bars["features"][closest]["properties"]["Attributes"]["Name"]
     print("Самый близкий бар - %s" % bar_name)
 
 
-if __name__ == '__main__':
-    bars = load_data('1796.json')
+if __name__ == "__main__":
+    bars = load_data("1796.json")
     longitude = float(input("Введите вашу геопозицию(долготу): "))
     latitude = float(input("Введите вашу геопозицию(широту): "))
 
